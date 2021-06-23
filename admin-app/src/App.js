75896a1f26cc4005d962609b88b57d4ad89da23a
Elvis Peralta <51756469+elvisperaltad@users.noreply.github.com>
2021-06-23 13:20:38 -0400

    ok

---
+++ admin-app/src/App.js
@@ -0,0 +1,47 @@
+import Sidebar from "./Components/sidebar/Sidebar";
+import Topbar from "./Components/topbar/Topbar";
+import "./app.css";
+import Home from "./Pages/home/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./Pages/userList/UserList";
+import User from "./Pages/user/User";
+import NewUser from "./Pages/newUser/NewUser";
+import ProductList from "./Pages/productList/ProductList";
+import Product from "./Pages/product/Product";
+import NewProduct from "./Pages/newProduct/NewProduct";
+
+function App() {
+  return (
+    <Router>
+      <Topbar />
+      <div className="container">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList />
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
